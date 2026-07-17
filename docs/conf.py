38000be45b9231047d7967659ef8fb1037919283
BlueBixt <newssungoldentoday@gmail.com>
2026-07-17 14:45:21 +0800

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'Archa License'
+copyright = '2026, Archa Authors'
+author = 'Archa Authors'
+release = '1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_theme_options = {
+    'navigation_depth': 4,
+    'titles_only': False
+}
+
+html_title = "Archa License Docs"
+html_logo = None
